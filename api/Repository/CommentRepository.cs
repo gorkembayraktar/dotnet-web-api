aad1f4d7ecb36783ca4f26a8389556f85cccca43
gorkembayraktar <myemail>
2024-10-01 17:16:28 +0300

    Comment System

---
+++ api/Repository/CommentRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public CommentRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<Comment>> GetAllAsync()
+        {
+            return _context.Comments.ToListAsync();
+        }
+    }
+}
